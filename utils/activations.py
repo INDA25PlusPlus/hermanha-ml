4504a6e59c4a80d15ff585b259857ba1dbae9333
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2025-11-18 10:39:04 +0100

    loss functions, added softmax activation aswell

--- utils/activations.py
+++ utils/activations.py
@@ -4,5 +4,9 @@ def sigmoid(Z: np.ndarray) -> np.ndarray:
     return 1/(1 + np.exp(-Z))
 
 def softmax(Z: np.ndarray) -> np.ndarray:
-    # later improvement
-    pass
+    # later improvement apperently softmax is the best for classification well for
+    # probabilistic classification, as all values adds up to one. Sigmoid would give individual probabilities
+    # which would lead to a total probability less than or more than 1, which could be problamatic.
+
+    Z = Z-np.max(Z)
+    return np.exp(Z)/np.sum(np.exp(Z))
---
+++ utils/losses.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def mse(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    return np.square(np.subtract(y_true, y_pred)).mean()
+
+def rmse(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    return np.sqrt(mse(y_pred, y_true))
+
+def cross_entropy(y_pred, y_true) -> np.ndarray:
+    return -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
