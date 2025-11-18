61e3d822ea7fab370b811966b4449b2055c06e9e
hallalay <98226058+hallalay@users.noreply.github.com>
2025-11-18 17:27:24 +0100

    simple back propagation (#10)

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 from utils import *
 from model.layers import Dense
 from utils.data_loader import load_data
+from utils.losses import mse, mse_grad
+import numpy as np
 
 X_train, y_train, X_test, y_test = load_data()
 
-for i in range(1):
+dense_1 = Dense(X_train[0].shape[0], 100)
+dense_2 = Dense(100, 10)
 
-    dense_1 = Dense(X_train[i].shape[0], 100)
-    dense_2 = Dense(100, 10)
+epochs = 100
 
-    y = dense_1.forward(X_train[i])
-    y = dense_2.forward(y)
+def evaluate_accuracy(dense_1, dense_2, X, y_true):
+    correct = 0
+    for i in range(len(X)):
+        out = dense_1.forward(X[i])
+        out = dense_2.forward(out)
+        if np.argmax(out) == np.argmax(y_true[i]):
+            correct += 1
+    return correct / len(X)
 
-    print(y)
-    
+for i in range(epochs):
+    p = np.random.permutation(len(X_train))
+    X_train = X_train[p]
+    y_train = y_train[p]
+
+    for X, y in zip(X_train, y_train):
+
+        out = dense_1.forward(X)
+        out = dense_2.forward(out)
+
+        grad = mse_grad(out, y)
+
+        dX = dense_2.backward(grad, 0.01)
+        dX = dense_1.backward(dX, 0.01)
+
+    loss = mse(out, y)
+
+    acc = evaluate_accuracy(dense_1, dense_2, X_test[:1000], y_test[:1000])
+    print("epoch: ", i, "loss: ", loss, "Accuracy: ", acc)
+
+
+
--- model/layers.py
+++ model/layers.py
@@ -2,21 +2,41 @@ import numpy as np
 from utils.activations import softmax, sigmoid
 
 class Dense:
-    def __init__(self, input_dim: int, output_dim: int, activation: str = "softmax"):
+    def __init__(self, input_dim: int, output_dim: int, activation: str = "sigmoid"):
         self.W = 0.01 * np.random.randn(input_dim, output_dim)
         self.b = np.zeros(output_dim)
         self.activation = activation
+        self.X = None
+        self.Z = None
     
     def forward(self, X) -> np.ndarray:
-        wb = X @ self.W + self.b
+        self.X = X
+        self.Z = X @ self.W + self.b
         if self.activation == "sigmoid":
-            return(sigmoid(wb))
+            self.Y = sigmoid(self.Z)
         elif self.activation == "softmax":
-            return(softmax(wb))
+            self.Y = softmax(self.Z)
         else:
             raise Exception("activation function not allowed")
-        
+        return self.Y
     
-            
+    def backward(self, dA, learning_rate):
+        if self.activation == "sigmoid":
+            dZ = dA * self.Y * (1-self.Y)
+        elif self.activation == "softmax":
+            # TODO
+            pass
+        else: 
+            raise Exception("unsupported activation")
         
-    
+        X = self.X.reshape(1, -1)
+        dZ = dZ.reshape(1, -1)
+        
+        dW = X.T @ dZ
+        dB = np.sum(dZ, axis=0)
+        dX = dZ @ self.W.T
+
+        self.W -= learning_rate * dW
+        self.b -= learning_rate * dB
+
+        return dX
