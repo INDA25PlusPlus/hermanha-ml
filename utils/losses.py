91141e1da327017f87872f3fa931562c77541e84
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2025-11-18 17:27:01 +0100

    simple back propagation

--- utils/losses.py
+++ utils/losses.py
@@ -3,6 +3,9 @@ import numpy as np
 def mse(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
     return np.square(np.subtract(y_true, y_pred)).mean()
 
+def mse_grad(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+    return 2 * (y_pred - y_true) / y_pred.size
+
 def rmse(y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
     return np.sqrt(mse(y_pred, y_true))
 
